524ba5d085097ba118135178d71ab5d2ca81e7bd
Bharat Soni <bharatchills@gmail.com>
2023-02-19 09:10:26 +0530

    added meta title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import React from 'react'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+import Head from 'next/head';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+       <Head>
+        <title>
+          Whatsapp Visualizer
+        </title>
+      </Head>
+      {" "}
+      <Component {...pageProps} />
+    </>
+  );
 }
